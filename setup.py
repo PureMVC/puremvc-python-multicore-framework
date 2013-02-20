8820cf1b23f18a3050cbf0e182bf469a2c306537
Daniele Esposti <de@marinesoftware.co.uk>
2013-02-20 11:50:51 +0000

    Updated revision number and changelog

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='PureMVC Multicore Python',
-      version='1.0.0',
+      version='1.0.1',
       description='PureMVC Multicore Python Framework',
       author='Toby de Havilland, Daniele Esposti',
       author_email='toby.de.havilland@puremvc.org, expo@expobrain.net',
